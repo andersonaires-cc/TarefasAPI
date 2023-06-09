40c1f548f34bd26e59e0cd7f58af840b32f9c7c3
anderson <andersonairesspp@hotmail.com>
2023-06-09 09:19:34 -0300

    Added Endpoints POST, PUT and DELETE

--- TarefasApi/Endpoints/TarefasEndpoints.cs
+++ TarefasApi/Endpoints/TarefasEndpoints.cs
@@ -19,6 +19,38 @@ namespace TarefasApi.Endpoints
                     return Results.NotFound();
                 return Results.Ok(tarefas);
             });
+
+            app.MapGet("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+
+                return con.Get<Tarefa>(id) is Tarefa tarefa ? Results.Ok(tarefa): Results.NotFound();
+            });
+
+            app.MapPost("/tarefas", async (GetConnection connectionGetter, Tarefa Tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Insert(Tarefa);
+                return Results.Created($"/tarefas/{id}", Tarefa);
+            });
+
+            app.MapPut("/tarefas", async (GetConnection connectionGetter, Tarefa Tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Update(Tarefa);
+                return Results.Ok();
+            });
+
+            app.MapDelete("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                var deleted = con.Get<Tarefa>(id);
+
+                if(deleted is null)
+                    return Results.NotFound();
+                con.Delete(deleted);
+                return Results.Ok(deleted);
+            });
         }
     }
 }
